f79af98fa8d640eae5e7387fd1b1edefee10743b
314141515 <digterstroke113@gmail.com>
2026-06-12 11:15:57 +0200

    card added

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function completeTodo(id) {
   if (todo) { todo.done = true; console.log(`Completed: "${todo.text}"`); }
 }
 
-addTodo("Learn Git");
+addTodo("Learn Github");
 addTodo("Build Todo App");
 listTodos();
 
